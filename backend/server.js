b67ea1823e3c01b2d0349b756134f5b959a12e06
Vimalan Christopher <117967331+Vimalanchristopher@users.noreply.github.com>
2025-02-12 01:43:15 +0530

    Fixed reset password issue and updated API URLs

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,8 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Backend is running...");
-  });
+});
   
-
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected Successfully"))
