b8e3b9a6234bcd9c9b1efda2b0c00e7fdf60e7b6
Vimalan Christopher <117967331+Vimalanchristopher@users.noreply.github.com>
2025-02-11 15:25:15 +0530

    Fixed the url in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5001/api/auth";
+const API_BASE_URL = "https://password-reset-pro.onrender.com/api/auth";
 
 export const forgotPassword = async (email) => {
   try {
