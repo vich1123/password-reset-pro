b15abf1549b881ef20bbeaf3e33889f8977df68a
Vimalan Christopher <117967331+Vimalanchristopher@users.noreply.github.com>
2025-02-11 12:01:23 +0530

    Added default route for Render

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Backend is running...");
+  });
+  
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected Successfully"))
